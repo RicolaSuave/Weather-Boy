d7d442c41883cc4c42cc71066913adb61b2a1dab
Rico Laureano <ricomateo711@gmail.com>
2023-02-02 07:27:00 -0800

    Weather Boy Copied, Will Modify Later

--- Weather-Boy2.py
+++ Weather-Boy2.py
@@ -1,32 +1,53 @@
-import json
 from tkinter import *
-from datetime import datetime
 import requests
-
+import json
+from datetime import datetime
+ 
+#Initialize Window
+ 
 root =Tk()
 root.geometry("400x400") #size of the window by default
 root.resizable(0,0) #to make the window size fixed
 #title of our window
 root.title("Weather App - AskPython.com")
-
+ 
+ 
+# ----------------------Functions to fetch and display weather info
 city_value = StringVar()
-
+ 
+ 
+def time_format_for_location(utc_with_tz):
+    local_time = datetime.utcfromtimestamp(utc_with_tz)
+    return local_time.time()
+ 
+ 
+city_value = StringVar()
+ 
 def showWeather():
-    #unique API key
-    api_key = "0a76d2ce307240904f9dbf5d7901ac51"
-    #User Input Later
-    city_value=city_value.get()
-    #API Url
+    #Enter you api key, copies from the OpenWeatherMap dashboard
+    api_key = "0a76d2ce307240904f9dbf5d7901ac51"  #sample API
+ 
+    # Get city name from user from the input field (later in the code)
+    city_name=city_value.get()
+ 
+    # API url
     weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid='+api_key
-    #Get response from fetch URL
+ 
+    # Get the response from fetched url
     response = requests.get(weather_url)
-    #Response changed to python readable format
-    weather_info = response.json
-    #Clears field
-    tfield.delete("1.0", "end")
-
+ 
+    # changing response from json to python readable 
+    weather_info = response.json()
+ 
+ 
+    tfield.delete("1.0", "end")   #to clear the text field for every new output
+ 
+#as per API documentation, if the cod is 200, it means that weather data was successfully fetched
+ 
+ 
     if weather_info['cod'] == 200:
-        kelvin = 273
+        kelvin = 273 # value of kelvin
+ 
 #-----------Storing the fetched values of weather of a city
  
         temp = int(weather_info['main']['temp'] - kelvin)                                     #converting default kelvin value to Celcius
@@ -42,11 +63,15 @@ def showWeather():
  
         sunrise_time = time_format_for_location(sunrise + timezone)
         sunset_time = time_format_for_location(sunset + timezone)
-        
+ 
+#assigning Values to our weather varaible, to display as output
+         
         weather = f"\nWeather of: {city_name}\nTemperature (Celsius): {temp}°\nFeels like in (Celsius): {feels_like_temp}°\nPressure: {pressure} hPa\nHumidity: {humidity}%\nSunrise at {sunrise_time} and Sunset at {sunset_time}\nCloud: {cloudy}%\nInfo: {description}"
     else:
         weather = f"\n\tWeather for '{city_name}' not found!\n\tKindly Enter valid City Name !!"
-    
+ 
+ 
+ 
     tfield.insert(INSERT, weather)   #to insert or send value in our Text Field to display output
  
  
@@ -69,4 +94,3 @@ tfield = Text(root, width=46, height=10)
 tfield.pack()
  
 root.mainloop()
- 
